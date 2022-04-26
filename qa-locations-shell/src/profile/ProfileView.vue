1832e39f9e522f564e429b519bb62ead8f3b023b
Dominik Haas <dominik.haas@qaware.de>
2022-04-26 13:27:49 +0200

    Added profile view based on web component

---
+++ qa-locations-shell/src/profile/ProfileView.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts"></script>
+<template>
+  <profile-details profile="1337">
+    <v-progress-circular></v-progress-circular>
+  </profile-details>
+</template>
--- qa-locations-shell/src/router/index.ts
+++ qa-locations-shell/src/router/index.ts
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
 import WelcomeView from "../welcome/WelcomeView.vue";
 
+import ProfileView from "../profile/ProfileView.vue";
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -8,6 +10,11 @@ const router = createRouter({
       path: "/",
       name: "welcome",
       component: WelcomeView,
+    },
+    {
+      path: "/profile",
+      name: "profile",
+      component: ProfileView,
     },
   ],
 });
