065a89effe7984a685aa474cade2e74b25ae7f01
Dominik Haas <dominik.haas@qaware.de>
2022-04-27 11:43:59 +0200

    Removed vuetify part from vite config

--- location/vite.config.ts
+++ location/vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vuetify from '@vuetify/vite-plugin'
 
 const path = require('path')
 
@@ -9,9 +8,6 @@ export default defineConfig({
   plugins: [
     vue(),
     // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
-    vuetify({
-      autoImport: true,
-    }),
   ],
   define: { 'process.env': {} },
   resolve: {
